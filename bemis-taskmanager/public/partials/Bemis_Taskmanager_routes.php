f7a0022ff5cb5a9bfcc3a0d22980b064366528e3
Umberto Calzari <umbertocalzari@gmail.com>
2019-05-21 22:33:45 +0200

    routes

--- bemis-taskmanager/public/partials/Bemis_Taskmanager_routes.php
+++ bemis-taskmanager/public/partials/Bemis_Taskmanager_routes.php
@@ -2,32 +2,31 @@
 
 if(isset($_GET['action'])){
     $action=$_GET['action'];
+    if($action=='myTasks'){
+        include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_myTasks.php';
+    } elseif($action=='newTask'){
+        include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_newTask.php';
+    } elseif($action=='lbgStatus'){
+        include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_lbgStatus.php';
+    } elseif($action=='projects'){
+        include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_projects.php';
+    } elseif($action=='newProject'){
+        include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_newProject.php';
+    } elseif($action=='newEvent'){
+        include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_newEvent.php';
+    } elseif($action=='editTask'){
+        include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_editTask.php';
+    } elseif($action=='editProject'){
+        include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_editProject.php';
+    } elseif($action=='editEvent'){
+        include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_editEvent.php';
+    } elseif($action=='deleteTask'){
+        include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_deleteTask.php';
+    } elseif($action=='deleteProject'){
+        include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_deleteProject.php';
+    } elseif($action=='deleteEvent'){
+        include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_deleteEvent.php';
+    }    
 } else {
     include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_mainPage.php';
-}
-
-if($action=='myTasks'){
-    include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_myTasks.php';
-} elseif($action=='newTask'){
-    include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_newTask.php';
-} elseif($action=='lbgStatus'){
-    include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_lbgStatus.php';
-} elseif($action=='projects'){
-    include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_projects.php';
-} elseif($action=='newProject'){
-    include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_newProject.php';
-} elseif($action=='newEvent'){
-    include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_newEvent.php';
-} elseif($action=='editTask'){
-    include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_editTask.php';
-} elseif($action=='editProject'){
-    include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_editProject.php';
-} elseif($action=='editEvent'){
-    include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_editEvent.php';
-} elseif($action=='deleteTask'){
-    include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_deleteTask.php';
-} elseif($action=='deleteProject'){
-    include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_deleteProject.php';
-} elseif($action=='deleteEvent'){
-    include plugin_dir_path(__FILE__).'pages/Bemis_Taskmanager_deleteEvent.php';
 }
