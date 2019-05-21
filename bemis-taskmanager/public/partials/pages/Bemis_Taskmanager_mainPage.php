524dc695a630512960d73370630228dabdd9a8cf
Umberto Calzari <umbertocalzari@gmail.com>
2019-05-21 22:44:45 +0200

    MainPage

--- bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_mainPage.php
+++ bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_mainPage.php
@@ -1,4 +1,9 @@
 <?php
+?>
 
+<div class="bemis-tm-mainpage-main-container">
+    <div class="bemis-tm-mainpage-sidebar"><?php include plugin_dir_path(__FILE__).'elements/Bemis_Taskmanager_sideBar.php'?></div>
+    <div class="bemis-tm-mainpage-content"></div>
+</div>
 
-echo '<h1>Main page</h1>';
+<?php
---
+++ bemis-taskmanager/public/partials/pages/elements/Bemis_Taskmanager_sideBar.php
@@ -0,0 +1,8 @@
+<?php 
+?>
+
+<div class="bemis-tm-sidebar-container">
+    <h1>Task Manager</h1>
+</div>
+
+<?
