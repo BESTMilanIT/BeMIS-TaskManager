4eaf151a8e5a02d646580ae3f562139f0ee6f747
Fabrizio Siciliano <sicilianofabrizio@gmail.com>
2019-05-21 23:33:31 +0200

    [Main page] - no rightsidebar element

--- bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_mainPage.php
+++ bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_mainPage.php
@@ -8,7 +8,20 @@
         CALENDAR
       </div>
       <div>
-          <?php include plugin_dir_path(__FILE__).'elements/Bemis_Taskmanager_rightSideBar.php' ?>
+        <ul>
+          <li class="bemis-tm-sidebar-main-menu-element">
+            <i class="material-icons">add_circle</i><span>Crea una nuova task</span>
+          </li>
+          <li class="bemis-tm-sidebar-main-menu-element">
+            <i class="material-icons">add_circle</i><span>Crea un nuovo progetto</span>
+          </li>
+          <li class="bemis-tm-sidebar-main-menu-element">
+            <i class="material-icons">add_circle</i><span>Crea un nuovo evento a calendario</span>
+          </li>
+          <li class="bemis-tm-sidebar-main-menu-element">
+            <i class="material-icons">add_circle</i><span>Mostra la situazione del gruppo</span>
+          </li>
+        </ul>
       </div>
     </div>
 </div>
