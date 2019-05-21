ebcaf279bfe06c5066f0cdb9e48c43166023f2c7
Umberto Calzari <umbertocalzari@gmail.com>
2019-05-21 22:19:19 +0200

    ops

--- bemis-taskmanager/public/partials/Bemis_Taskmanager_routes.php
+++ bemis-taskmanager/public/partials/Bemis_Taskmanager_routes.php
@@ -4,33 +4,33 @@ if(isset($_GET['action'])){
     $action=$_GET['action'];
 } else {
     echo "<h1>crisi</h1>";
-    plugin_dir_path(__FILE__).'partials/pages/main_page.php';
+    include plugin_dir_path(__FILE__).'partials/pages/main_page.php';
 }
 
 if($action=='myTasks'){
-    plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_myTasks.php';
+    include plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_myTasks.php';
 } elseif($action=='newTask'){
-    plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_newTask.php';
+    include plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_newTask.php';
 } elseif($action=='lbgStatus'){
-    plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_lbgStatus.php';
+    include plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_lbgStatus.php';
 } elseif($action=='projects'){
-    plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_projects.php';
+    include plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_projects.php';
 } elseif($action=='newProject'){
-    plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_newProject.php';
+    include plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_newProject.php';
 } elseif($action=='newEvent'){
-    plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_newEvent.php';
+    include plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_newEvent.php';
 } elseif($action=='editTask'){
-    plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_editTask.php';
+    include plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_editTask.php';
 } elseif($action=='editProject'){
-    plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_editProject.php';
+    include plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_editProject.php';
 } elseif($action=='editEvent'){
-    plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_editEvent.php';
+    include plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_editEvent.php';
 } elseif($action=='deleteTask'){
-    plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_deleteTask.php';
+    include plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_deleteTask.php';
 } elseif($action=='deleteProject'){
-    plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_deleteProject.php';
+    include plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_deleteProject.php';
 } elseif($action=='deleteEvent'){
-    plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_deleteEvent.php';
+    include plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_deleteEvent.php';
 } elseif($action=='editProject'){
-    plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_editProject.php';
+    include plugin_dir_path(__FILE__).'partials/pages/Bemis_Taskmanager_editProject.php';
 } 
