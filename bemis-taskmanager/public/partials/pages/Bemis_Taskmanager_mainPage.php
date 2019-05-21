56d00f2f9618216d43b852f13ee3637399d5014f
Fabrizio Siciliano <sicilianofabrizio@gmail.com>
2019-05-21 23:29:53 +0200

    [MainPage] - Right sidebar

--- bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_mainPage.php
+++ bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_mainPage.php
@@ -3,7 +3,14 @@
 
 <div class="bemis-tm-mainpage-main-container">
     <div class="bemis-tm-mainpage-sidebar"><?php include plugin_dir_path(__FILE__).'elements/Bemis_Taskmanager_sideBar.php' ?></div>
-    <div class="bemis-tm-mainpage-content"></div>
+    <div class="bemis-tm-mainpage-content">
+      <div class="bemis-tm-mainpage-calendar-container">
+        CALENDAR
+      </div>
+      <div>
+          <?php include plugin_dir_path(__FILE__).'elements/Bemis_Taskmanager_rightSideBar.php' ?>
+      </div>
+    </div>
 </div>
 
 <?php
---
+++ bemis-taskmanager/public/partials/pages/elements/Bemis_Taskmanager_rightSideBar.php
@@ -0,0 +1,19 @@
+<?php
+?>
+
+<div class="bemis-tm-sidebar-main-menu-container">
+    <ul class="bemis-tm-sidebar-main-menu">
+      <li class="bemis-tm-sidebar-main-menu-element">
+        <i class="material-icons">add_circle</i><span>Crea una nuova task</span>
+      </li>
+      <li class="bemis-tm-sidebar-main-menu-element">
+        <i class="material-icons">add_circle</i><span>Crea un nuovo progetto</span>
+      </li>
+      <li class="bemis-tm-sidebar-main-menu-element">
+        <i class="material-icons">add_circle</i><span>Crea un nuovo evento a calendario</span>
+      </li>
+      <li class="bemis-tm-sidebar-main-menu-element">
+        <i class="material-icons">add_circle</i><span>Mostra la situazione del gruppo</span>
+      </li>
+    </ul>
+</div>
