5590783be2213f561d01d9a4d16dfccbd39a97e2
Fabrizio Siciliano <sicilianofabrizio@gmail.com>
2019-05-21 22:57:27 +0200

    [Gmaterial] - style and scripts

--- bemis-taskmanager/public/class-bemis-taskmanager-public.php
+++ bemis-taskmanager/public/class-bemis-taskmanager-public.php
@@ -73,8 +73,8 @@ class Bemis_Taskmanager_Public {
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
-		 wp_enqueue_style('gmaterial_style', 'https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css', array(), $this->version, 'all' );
-
+		 wp_enqueue_style('gmaterial_style_1', 'https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css', array(), $this->version, 'all' );
+		 wp_enqueue_style('gmaterial_style_2', 'https://fonts.googleapis.com/icon?family=Material+Icons', array(), $this->version, 'all' );
 		 wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/bemis-taskmanager-public.css', array(), $this->version, 'all' );
 
 	}
@@ -97,7 +97,7 @@ class Bemis_Taskmanager_Public {
 		 * between the defined hooks and the functions defined in this
 		 * class.
 		 */
-
+		 wp_enqueue_script('gmaterial_script', 'https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js', array( 'jquery' ), $this->version, false)
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/bemis-taskmanager-public.js', array( 'jquery' ), $this->version, false );
 
 	}
