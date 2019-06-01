9a70058da949223121499f856acdf45f9e9c50ac
Fabrizio Siciliano <sicilianofabrizio@gmail.com>
2019-06-01 10:44:46 +0200

    Fix

---
+++ bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_newTask.php
@@ -0,0 +1,3 @@
+<div class="bemis-tm-mainpage-main-container">
+  <div class="bemis-tm-mainpage-sidebar"><?php include plugin_dir_path(__FILE__).'elements/Bemis_Taskmanager_sideBar.php' ?></div>
+</div>
--- bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_projects.php
+++ bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_projects.php
@@ -1,3 +1,1 @@
-<div>
-  FANFU CULO
-</div>
+
