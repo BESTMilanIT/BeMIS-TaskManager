a3e9a8e8ce4bc6e2265808ea9e63662cd12b1e93
Fabrizio Siciliano <sicilianofabrizio@gmail.com>
2019-06-04 22:09:39 +0200

    Setting main page routes

--- bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_mainPage.php
+++ bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_mainPage.php
@@ -8,18 +8,24 @@
       <div class="bemis-tm-mainpage-rightsidebar">
         <ul>
           <li style="padding-bottom: 2em;" class="bemis-tm-sidebar-main-menu-element">
-            <a href="/task-manager-2?action=newTask"> 
+            <a href="/task-manager-2?action=newTask">
               <i class="material-icons bemis-tm-sidebar-main-menu-icon">add_circle</i><span>Crea una nuova task</span>
             </a>
           </li>
           <li style="padding-bottom: 2em;" class="bemis-tm-sidebar-main-menu-element">
-            <i class="material-icons bemis-tm-sidebar-main-menu-icon">add_circle</i><span>Crea un nuovo progetto</span>
+            <a href="/task-manager-2?action=newProject">
+              <i class="material-icons bemis-tm-sidebar-main-menu-icon">add_circle</i><span>Crea un nuovo progetto</span>
+            </a>
           </li>
-          <li style="padding-bottom: 2em;" class="bemis-tm-sidebar-main-menu-element">
+          <a href="/task-manager-2?action=newEvent">
+            <li style="padding-bottom: 2em;" class="bemis-tm-sidebar-main-menu-element">
+          </a>
             <i class="material-icons bemis-tm-sidebar-main-menu-icon">add_circle</i><span>Crea un nuovo evento a calendario</span>
           </li>
           <li class="bemis-tm-sidebar-main-menu-element">
-            <i class="material-icons bemis-tm-sidebar-main-menu-icon">add_circle</i><span>Mostra la situazione del gruppo</span>
+            <a href="/task-manager-2?action=lbgStatus"> 
+              <i class="material-icons bemis-tm-sidebar-main-menu-icon">add_circle</i><span>Mostra la situazione del gruppo</span>
+            </a>
           </li>
         </ul>
       </div>
--- bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_newTask.php
+++ bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_newTask.php
@@ -1,3 +1,7 @@
 <div class="bemis-tm-mainpage-main-container">
   <div class="bemis-tm-mainpage-sidebar"><?php include plugin_dir_path(__FILE__).'elements/Bemis_Taskmanager_sideBar.php' ?></div>
+
+  <div>
+    New Task
+  </div>
 </div>
