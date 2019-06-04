aed343553d6a7b6d63e6975b816823626283c256
Fabrizio Siciliano <sicilianofabrizio@gmail.com>
2019-06-04 22:24:28 +0200

    Sidebar routes 75%

--- bemis-taskmanager/public/partials/pages/elements/Bemis_Taskmanager_sideBar.php
+++ bemis-taskmanager/public/partials/pages/elements/Bemis_Taskmanager_sideBar.php
@@ -2,16 +2,22 @@
     <h1>Task Manager</h1>
         <ul class="bemis-tm-sidebar-main-menu">
             <li class="bemis-tm-sidebar-main-menu-element">
+              <a href="/task-manager-2">
                 <i class="material-icons bemis-tm-sidebar-main-menu-icon">home</i><span>Home</span>
+              </a>
             </li>
             <li class="bemis-tm-sidebar-main-menu-element">
                 <i class="material-icons bemis-tm-sidebar-main-menu-icon">home</i><span>Timeline</span>
             </li>
             <li class="bemis-tm-sidebar-main-menu-element">
+              <a href="/task-manager-2?action=myTasks">
                 <i class="material-icons bemis-tm-sidebar-main-menu-icon">home</i><span>My Tasks</span>
+              </a>
             </li>
             <li class="bemis-tm-sidebar-main-menu-element">
+              <a href="/task-manager-2?action=projects">
                 <i class="material-icons bemis-tm-sidebar-main-menu-icon">home</i><span>Progetti</span>
+              </a>
             </li>
         </ul>
 </div>
