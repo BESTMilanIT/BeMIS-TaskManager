885f1c41fe8457058c183835f0896b16c30aadee
Fabrizio Siciliano <sicilianofabrizio@gmail.com>
2019-06-04 20:33:15 +0200

    rightsidebar restyle

--- bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_mainPage.php
+++ bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_mainPage.php
@@ -2,17 +2,18 @@
     <div class="bemis-tm-mainpage-sidebar"><?php include plugin_dir_path(__FILE__).'elements/Bemis_Taskmanager_sideBar.php' ?></div>
     <div class="bemis-tm-mainpage-content">
       <div class="bemis-tm-mainpage-calendar-container">
+
         <?php echo do_shortcode('[calendar id="54"]')?>
       </div>
       <div class="bemis-tm-mainpage-rightsidebar">
         <ul>
-          <li class="bemis-tm-sidebar-main-menu-element">
-            <i href="https://bemis.bestmilano.it/task-manager-2?newTask" class="material-icons bemis-tm-sidebar-main-menu-icon" >add_circle</i><span>Crea una nuova task <?php echo $_GET['newTask'] ?></span>
+          <li style="padding-bottom: 2em;" class="bemis-tm-sidebar-main-menu-element">
+            <i href="https://bemis.bestmilano.it/task-manager-2/newTask" class="material-icons bemis-tm-sidebar-main-menu-icon" >add_circle</i><span>Crea una nuova task <?php echo $_GET['newTask'] ?></span>
           </li>
-          <li class="bemis-tm-sidebar-main-menu-element">
+          <li style="padding-bottom: 2em;" class="bemis-tm-sidebar-main-menu-element">
             <i class="material-icons bemis-tm-sidebar-main-menu-icon">add_circle</i><span>Crea un nuovo progetto</span>
           </li>
-          <li class="bemis-tm-sidebar-main-menu-element">
+          <li style="padding-bottom: 2em;" class="bemis-tm-sidebar-main-menu-element">
             <i class="material-icons bemis-tm-sidebar-main-menu-icon">add_circle</i><span>Crea un nuovo evento a calendario</span>
           </li>
           <li class="bemis-tm-sidebar-main-menu-element">
