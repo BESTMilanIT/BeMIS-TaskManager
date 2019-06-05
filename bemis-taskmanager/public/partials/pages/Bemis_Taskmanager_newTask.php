ad3ce4b16f3a75be2cfc49365b4ee9ae44f94209
Fabrizio Siciliano <sicilianofabrizio@gmail.com>
2019-06-05 21:18:47 +0200

    Fuck

--- bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_newTask.php
+++ bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_newTask.php
@@ -3,6 +3,10 @@
   <div class="bemis-tm-mainpage-sidebar"><?php include plugin_dir_path(__FILE__).'elements/Bemis_Taskmanager_sideBar.php' ?></div>
 
   <div class="bemis-tm-mainpage-rightsidebar">
-    <input class="mdc-text-field__input" type="text" placeholder="Type a task name... " aria-label="Task Name">Task Name</input>
+    <ul>
+      <li>
+        <label>Task Name</label><input class="mdc-text-field__input" type="text" placeholder="Type a task name... " aria-label="Task Name"></input>
+      </li>
+    </ul>
   </div>
 </div>
