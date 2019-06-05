1bff9f43f3c53d039616fc1eaf6663615e41919f
Fabrizio Siciliano <sicilianofabrizio@gmail.com>
2019-06-05 20:30:30 +0200

    Fix rightsidebar

--- bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_newTask.php
+++ bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_newTask.php
@@ -2,11 +2,9 @@
   <!--sidebar needs always to be there-->
   <div class="bemis-tm-mainpage-sidebar"><?php include plugin_dir_path(__FILE__).'elements/Bemis_Taskmanager_sideBar.php' ?></div>
 
-  <div class="bemis-tm-mainpage-main-container">
-    <form>
-      <input type="text" id="task-name" class="mdc-text-field__input">
-      <label class="mdc-floating-label" for="task-name"><h1>Task Name</h1></label>
-      <div class="mdc-line-ripple"></div>
-    </form>
+  <div class="bemis-tm-mainpage-rightsidebar">
+    <input type="text" id="task-name" class="mdc-text-field__input">
+    <label class="mdc-floating-label" for="task-name"><h1>Task Name</h1></label>
+    <div class="mdc-line-ripple"></div>
   </div>
 </div>
