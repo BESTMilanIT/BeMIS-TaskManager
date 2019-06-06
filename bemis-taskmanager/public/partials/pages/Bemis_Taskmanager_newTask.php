2d3a6f9db2e2180bb367645ad359840e4c4a0164
Fabrizio Siciliano <sicilianofabrizio@gmail.com>
2019-06-06 14:13:10 +0200

    Fix

--- bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_newTask.php
+++ bemis-taskmanager/public/partials/pages/Bemis_Taskmanager_newTask.php
@@ -5,7 +5,8 @@
   <div class="bemis-tm-mainpage-rightsidebar">
     <ul>
       <li class="bemis-tm-form-element-container">
-          <label for="input-task-name"><h1>Task Name<h1></label><input id="input-task-name"class="mdc-text-field__input" type="text" placeholder="Type a task name... " aria-label="Task Name"></input>
+          <label for="input-task-name"><h1>Task Name</h1></label>
+          <input id="input-task-name"class="mdc-text-field__input" type="text" placeholder="Type a task name... " aria-label="Task Name"></input>
       </li>
       <li class="bemis-tm-form-element-container">
         <label>Projects</label>
